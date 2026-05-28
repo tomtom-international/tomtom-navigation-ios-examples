06dfeefd92e451d99b61993f8eb2f93d036b7b24
TomTom SDK Publication App <no-reply@tomtom.com>
2026-05-28 11:55:40 +0000

    TomTom SDK Release 0.73.0

--- TomTomNavigationCoreExamples/SPM/ProxyPackage/Package.swift
+++ TomTomNavigationCoreExamples/SPM/ProxyPackage/Package.swift
@@ -13,7 +13,7 @@
 import PackageDescription
 
 /// The version for all TomTom SDK dependencies.
-let sdkVersion: Version = "0.72.1"
+let sdkVersion: Version = "0.73.0"
 
 /// Dictionary of all dependencies.
 /// `key`: package name.
