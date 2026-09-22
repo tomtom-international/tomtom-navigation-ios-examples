c8aedc2e624cfdca876b2df4c94e79fe4c7bbf1e
TomTom SDK Publication App <no-reply@tomtom.com>
2026-09-22 09:09:30 +0000

    TomTom SDK Release 0.74.2

--- TomTomNavigationCoreExamples/TomTomSDKExamples/Examples/UseCase/OnlineNavigationContent.swift
+++ TomTomNavigationCoreExamples/TomTomSDKExamples/Examples/UseCase/OnlineNavigationContent.swift
@@ -147,6 +147,9 @@ extension NavigationController: NavigationRouteAddObserver {
 struct MainView: View {
     // MARK: Internal
 
+    @StateObject
+    var navigationController = NavigationController()
+
     var body: some View {
         ZStack(alignment: .bottom) {
             TomTomMapView(contentInsets: $contentInsets, navigationController: navigationController)
@@ -166,9 +169,6 @@ struct MainView: View {
 
     @State
     private var contentInsets = EdgeInsets()
-
-    @StateObject
-    var navigationController = NavigationController()
 }
 
 // MARK: - TomTomMapView
